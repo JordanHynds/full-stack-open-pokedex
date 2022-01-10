7948edbb01bc000b6de399de95b68051c9d45641
JordanHynds <jordanroberthynds@gmail.com>
2022-01-10 11:21:30 -0800

    testing slack-notification

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
-  url,
+  ur,
   name,
   id: parseInt(url.match(/\/(\d+)\//)[1])
 })))
